5c4b7565ae230bff07da5ec24bfb0b3bd577a307
root <root@debian>
2016-05-16 17:23:52 +0200

    first commit

---
+++ first.c
@@ -0,0 +1,41 @@
+/* =====================================================================================
+ *
+ *       Filename:  first.c
+ *
+ *    Description:  This is the first c file
+ *
+ *        Version:  1.0
+ *        Created:  01.06.2013 21:59:18
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Moritz Broghammer (), MoritzBroghammer@web.de
+ *        Company:  
+ *
+ * ===================================================================================*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+int main ( int argc, char *argv[] )
+{
+    uint32_t u32Test = 0;
+    
+        
+	printf ( "Hallo World!\n" );
+    u32Test = 1;
+    printf ( "Hallo Janina!\n" );
+    
+    u32Test = 2;
+    
+	return u32Test;
+}				/* ----------  end of function main  ---------- */
+
+
