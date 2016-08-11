b39fdcbfcc3b1e438aae323ed6dc58c4ffac6b0c
benjamin.hug <famhug@gmx.de>
2016-08-11 12:13:48 +0200

    New print module

---
+++ src/fos/print.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  print.c
+ *
+ *    Description:  Call the printf function
+ *
+ *        Version:  1.0
+ *        Created:  10.08.2016 23:39:18
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include "print.h"
+
+/* ===  FUNCTION  ======================================================================
+*         Name:  print
+*  Description:  
+* =====================================================================================
+                 */
+void print(void)
+{
+  printf("Hello world!\n");
+}                               /* -----  end of function print  ----- */
