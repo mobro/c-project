455ba61a5c0b45d4e181e10fc5872de98517b5f4
ben <ben@debian>
2016-10-26 22:07:14 +0200

    include folders app, fw and pf

---
+++ app/src/fos/config.h
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  config.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10.08.2016 19:49:32
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+// The defines come from the CMakeLists.txt file
+#define C_PROJECT_VERSION_MAJOR @C_PROJECT_VERSION_MAJOR@
+#define C_PROJECT_VERSION_MINOR @C_PROJECT_VERSION_MINOR@
+
---
+++ fw/src/fos/math/test/testsqroot.cpp
@@ -0,0 +1,46 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  testsqroot.cpp
+ *
+ *    Description:  /
+ *
+ *        Version:  1.0
+ *        Created:  10/26/2016 12:30:24 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Moritz Broghammer (), MoritzBroghammer@web.de
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "../../../src/fos/math/src/sqroot.c"
+#include <gtest/gtest.h>
+
+TEST(SquareRootTest, NegativeNos)
+{
+  ASSERT_EQ(-1.0, squareRoot(-15.0));
+  ASSERT_EQ(-1.0, squareRoot(-0.2));
+}
+
+TEST(SquareRootTest, PositiveNos)
+{
+  ASSERT_EQ(6, squareRoot(36.0));
+  ASSERT_EQ(18.0, squareRoot(324.0));
+  ASSERT_EQ(25.4, squareRoot(645.16));
+  ASSERT_EQ(0, squareRoot(0.0));
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}                               /* ----------  end of function main  ---------- */
