59d56295b8f8ce193e40c878918f00b81a0a8b5b
ben <ben@debian>
2016-10-26 22:34:48 +0200

    Modified

--- BuildAndExecute.sh
+++ BuildAndExecute.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # This is a comment!
 echo cmake is called followd by the make command and finally the compiled programm is executed        # This is a comment, too!
+#cmake . -G "Visual Studio 10 Win64"
 cmake .
 make
 cd build
