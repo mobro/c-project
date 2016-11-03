2d904cc65ab9e20b2ed60e912ce1740f0f859900
mobro <famhug@gmx.de>
2016-11-03 11:32:08 +0100

    update

--- fw/src/fos/corefnct/math/test/testsmultiplication.cpp
+++ fw/src/fos/corefnct/math/test/testsmultiplication.cpp
@@ -16,21 +16,21 @@
  * =====================================================================================
  */
 #include <stdlib.h>
-#include "../src/sqroot.c"
+#include "../src/fw_mulitplication.c"
 #include <gtest/gtest.h>
 
-TEST(SquareRootTest, NegativeNos)
+TEST(MulitplicationTest, NegativeNos)
 {
-  ASSERT_EQ(-1.0, squareRoot(-15.0));
-  ASSERT_EQ(-1.0, squareRoot(-0.2));
+  ASSERT_EQ(-1.0, umult32(-15.0));
+  ASSERT_EQ(-1.0, umult32(-0.2));
 }
 
-TEST(SquareRootTest, PositiveNos)
+TEST(MulitplicationTest, PositiveNos)
 {
-  ASSERT_EQ(6, squareRoot(36.0));
-  ASSERT_EQ(18.0, squareRoot(324.0));
-  ASSERT_EQ(25.4, squareRoot(645.16));
-  ASSERT_EQ(0, squareRoot(0.0));
+  ASSERT_EQ(6, umult32(36.0));
+  ASSERT_EQ(18.0, umult32(324.0));
+  ASSERT_EQ(25.4, umult32(645.16));
+  ASSERT_EQ(0, umult32(0.0));
 }
 
 /* 
