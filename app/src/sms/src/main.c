7d046531f6cb22e421232df1fec8776057808186
mobro <famhug@gmx.de>
2016-12-29 17:56:56 +0100

    use fork in main

--- app/src/sms/src/main.c
+++ app/src/sms/src/main.c
@@ -17,6 +17,7 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <fos/math/sqroot.h>
 /* 
  * ===  FUNCTION  ======================================================================
@@ -27,7 +28,39 @@
 
 int main(int argc, char *argv[])
 {
-  double dSquareRoot = 2.0;
-  dSquareRoot = squareRoot(1024);
-  printf("The square root of 2 is %f!\n", dSquareRoot);
+  int32_t i32Cntr = 0;
+  printf("Beginning of program\n");
+#ifdef __linux__
+  printf("You are running under linux\n");
+#elif _WIN32
+  printf("You are running under windows\n");
+#else
+#endif
+
+  pid_t pid = fork();
+
+  if (0 == pid)
+  {
+    int32_t i32Loop = 0;
+    for (i32Loop = 0; 50 > i32Loop; i32Loop++)
+    {
+      printf("child process: counter value is %d\n", ++i32Cntr);
+    }
+  }
+  else if (0 < pid)
+  {
+    int32_t i32Loop = 0;
+    for (i32Loop = 0; 50 > i32Loop; i32Loop++)
+    {
+      printf("parent process: counter value is %d\n", ++i32Cntr);
+    }
+  }
+  else
+  {
+    printf("fork failed\n");
+  }
+
+  printf("End of program\n");
+
+  return 0;
 }                               /* ----------  end of function main  ---------- */
